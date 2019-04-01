b50f9e1a2b88c6aa018e54834f3bc997760226d2
Tomanhez <tomek.sut@gmail.com>
2019-04-01 14:47:09 +0200

    Add new functions in ShippingMethod.php

--- src/Entity/Shipping/ShippingMethod.php
+++ src/Entity/Shipping/ShippingMethod.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace App\Entity\Shipping;
 
-use Doctrine\ORM\Mapping\MappedSuperclass;
-use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\ShippingMethod as BaseShippingMethod;
 use Sylius\Component\Shipping\Model\ShippingMethodTranslationInterface;
 
 /**
- * @MappedSuperclass
- * @Table(name="sylius_shipping_method")
+ * @ORM\MappedSuperclass()
+ * @ORM\Table(name="sylius_shipping_method")
  */
 class ShippingMethod extends BaseShippingMethod
 {
+    /** @ORM\Column(type="string",nullable=true) */
+    private $estimatedDeliveryTime;
+
+    public function getEstimatedDeliveryTime(): ?string
+    {
+        return $this->estimatedDeliveryTime;
+    }
+
+    public function setEstimatedDeliveryTime(string $estimatedDeliveryTime): void
+    {
+        $this->estimatedDeliveryTime = $estimatedDeliveryTime;
+    }
+
     protected function createTranslation(): ShippingMethodTranslationInterface
     {
         return new ShippingMethodTranslation();
