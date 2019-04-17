db89409108c7031f0adbd25bdfd4bb3f025270a8
Tomanhez <tomek.sut@gmail.com>
2019-04-17 14:26:17 +0200

    Add new functions in Country

--- src/Entity/Addressing/Country.php
+++ src/Entity/Addressing/Country.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace App\Entity\Addressing;
 
-use Doctrine\ORM\Mapping\MappedSuperclass;
-use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Addressing\Model\Country as BaseCountry;
+use Sylius\Component\Addressing\Model\CountryInterface;
 
 /**
- * @MappedSuperclass
- * @Table(name="sylius_country")
+ * @ORM\Entity()
+ * @ORM\Table(name="sylius_country")
  */
-class Country extends BaseCountry
+class Country extends BaseCountry implements CountryInterface
 {
+    /** @ORM\Column(type="string", nullable=true) */
+    private $flag;
+
+    public function getFlag(): ?string
+    {
+        return $this->flag;
+    }
+
+    public function setFlag(string $flag): void
+    {
+        $this->flag = $flag;
+    }
 }
