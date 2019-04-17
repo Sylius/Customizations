61984e2fdb480d175417777d5a5b7289221e1708
Tomanhez <tomek.sut@gmail.com>
2019-04-17 15:18:31 +0200

    Add new function in ShippingMethod.php and ShippingMethodTranslation.php

--- src/Entity/Shipping/ShippingMethod.php
+++ src/Entity/Shipping/ShippingMethod.php
@@ -4,17 +4,27 @@ declare(strict_types=1);
 
 namespace App\Entity\Shipping;
 
-use Doctrine\ORM\Mapping\MappedSuperclass;
-use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\ShippingMethod as BaseShippingMethod;
+use Sylius\Component\Core\Model\ShippingMethodInterface;
 use Sylius\Component\Shipping\Model\ShippingMethodTranslationInterface;
 
 /**
- * @MappedSuperclass
- * @Table(name="sylius_shipping_method")
+ * @ORM\Entity()
+ * @ORM\Table(name="sylius_shipping_method")
  */
-class ShippingMethod extends BaseShippingMethod
+class ShippingMethod extends BaseShippingMethod implements ShippingMethodInterface
 {
+    public function getDeliveryConditions(): ?string
+    {
+        return $this->getTranslation()->getDeliveryConditions();
+    }
+
+    public function setDeliveryConditions(?string $deliveryConditions): void
+    {
+        $this->getTranslation()->setDeliveryConditions($deliveryConditions);
+    }
+
     protected function createTranslation(): ShippingMethodTranslationInterface
     {
         return new ShippingMethodTranslation();
--- src/Entity/Shipping/ShippingMethodTranslation.php
+++ src/Entity/Shipping/ShippingMethodTranslation.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace App\Entity\Shipping;
 
-use Doctrine\ORM\Mapping\MappedSuperclass;
-use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Shipping\Model\ShippingMethodTranslation as BaseShippingMethodTranslation;
+use Sylius\Component\Shipping\Model\ShippingMethodTranslationInterface;
 
 /**
- * @MappedSuperclass
- * @Table(name="sylius_shipping_method_translation")
+ * @ORM\Entity()
+ * @ORM\Table(name="sylius_shipping_method_translation")
  */
-class ShippingMethodTranslation extends BaseShippingMethodTranslation
+class ShippingMethodTranslation extends BaseShippingMethodTranslation implements ShippingMethodTranslationInterface
 {
+    /** @ORM\Column(type="string", nullable=true) */
+    private $deliveryConditions;
+
+    public function getDeliveryConditions(): ?string
+    {
+        return $this->deliveryConditions;
+    }
+
+    public function setDeliveryConditions(?string $deliveryConditions): void
+    {
+        $this->deliveryConditions = $deliveryConditions;
+    }
 }
