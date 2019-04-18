7d2ee2bead3251d119f3ad9b29184a8e386c33e4
Tomanhez <tomek.sut@gmail.com>
2019-04-18 09:36:05 +0200

    Add new function in Customer

--- src/Entity/Customer/Customer.php
+++ src/Entity/Customer/Customer.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace App\Entity\Customer;
 
-use Doctrine\ORM\Mapping\MappedSuperclass;
-use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Customer as BaseCustomer;
+use Sylius\Component\Core\Model\CustomerInterface;
 
 /**
- * @MappedSuperclass
- * @Table(name="sylius_customer")
+ * @ORM\Entity()
+ * @ORM\Table(name="sylius_customer")
  */
-class Customer extends BaseCustomer
+class Customer extends BaseCustomer implements CustomerInterface
 {
+    /** @ORM\Column(type="string",nullable=true) */
+    private $secondaryPhoneNumber;
+
+    public function getSecondaryPhoneNumber(): ?string
+    {
+        return $this->secondaryPhoneNumber;
+    }
+
+    public function setSecondaryPhoneNumber(string $secondaryPhoneNumber): void
+    {
+        $this->secondaryPhoneNumber = $secondaryPhoneNumber;
+    }
 }
