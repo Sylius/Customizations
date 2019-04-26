24cd44012d09e037adc78101c7e0abcddf528d69
Adam Kasperczak <adamkasperczak123@gmail.com>
2019-04-26 15:35:29 +0200

    How to customize Admin Menu?

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function addAdminMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $newSubmenu = $menu ->addChild('new')->setLabel('Custom Admin Submenu');
+
+        $newSubmenu->addChild('new-subitem')->setLabel('Custom Admin Menu Itemu');
+    }
+}
