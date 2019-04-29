70486ea6bec483da0d5f4b2be0635c176079087d
Adam Kasperczak <adamkasperczak123@gmail.com>
2019-04-29 09:18:48 +0200

    How to customize Account Menu?

---
+++ src/Menu/AccountMenuListener.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AccountMenuListener
+{
+    public function addAccountMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $menu
+            ->addChild('new', ['route' => 'sylius_shop_account_dashboard'])
+            ->setLabel('Custom Account Menu Item')
+            ->setLabelAttribute('icon', 'star')
+        ;
+    }
+}
